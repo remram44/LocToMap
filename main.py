f2048158d8b8e173027d2e99adbb6aad4b1e9a7d
semyon <github@semyon.eu>
2024-03-13 21:44:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from typing import Union
+
+from fastapi import FastAPI
+from starlette.responses import FileResponse 
+
+import dns.resolver
+import re
+
+app = FastAPI()
+COORD_PATTERN = r"(\d+) (\d+) (\d+\.\d+) ([NS]) (\d+) (\d+) (\d+\.\d+) ([EW])"
+
+def dms_to_decimal(degrees: int, minutes: int, seconds: float) -> float:
+    return degrees + minutes / 60 + seconds / 3600
+
+@app.get("/")
+async def read_index():
+    return FileResponse('map.html')
+
+@app.get("/api/{domain}")
+async def get_loc_record(domain: str) -> dict:
+    try:
+        response = dns.resolver.query(domain, "LOC")
+        loc_data = {}
+        for record in response:
+            full_record = str(record)
+            match = re.search(COORD_PATTERN, full_record)
+            if match:
+                latitude_degrees = int(match.group(1))
+                latitude_minutes = int(match.group(2))
+                latitude_seconds = float(match.group(3))
+                hemisphere_north_south = match.group(4)
+                longitude_degrees = int(match.group(5))
+                longitude_minutes = int(match.group(6))
+                longitude_seconds = float(match.group(7))
+                hemisphere_east_west = match.group(8)
+
+                latitude = dms_to_decimal(latitude_degrees, latitude_minutes, latitude_seconds)
+                if hemisphere_north_south == 'S':
+                    latitude *= -1
+
+                longitude = dms_to_decimal(longitude_degrees, longitude_minutes, longitude_seconds)
+                if hemisphere_east_west == 'W':
+                    longitude *= -1
+
+                loc_data["latitude"] = str(latitude)
+                loc_data["longitude"] = str(longitude)
+                loc_data['full'] = full_record
+        return loc_data
+    except Exception as e:
+        return {"error": str(e)}
